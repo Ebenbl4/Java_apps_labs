f3ae4ad415c89c21b7a242c9a6441676941d7f9a
Ebenbl4 <168077908+Ebenbl4@users.noreply.github.com>
2026-03-12 16:07:33 +0300

    Change types for constants and functions

--- src/minesweeperkurs/GameLogic.java
+++ src/minesweeperkurs/GameLogic.java
@@ -24,9 +24,9 @@ public class GameLogic {
     private final Random random = new Random();
 	private final List<List<CellState>> field;
 	private final List<List<Byte>> nearbyMineCounts;
-	private final int ROWS = 18;
-    private final int COLS = 30;
-    private final int MINES_COUNT = 99;
+	private final short ROWS = 18;
+    private final short COLS = 30;
+    private final short MINES_COUNT = 99;
     private int minesRemain = MINES_COUNT;
 	private boolean gameOver;
 	private boolean winCondition;
@@ -59,8 +59,8 @@ public class GameLogic {
 		Collections.shuffle(positionsList);
 		positionsList.stream().limit(MINES_COUNT).forEach(pos -> field.get(pos[0]).set(pos[1], CellState.CONTAINS_MINE));
 		
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < COLS; j++) {
+		for (short i = 0; i < ROWS; i++) {
+			for (short j = 0; j < COLS; j++) {
 				nearbyMineCounts.get(i).set(j, countNearbyMines(i, j));
 			}
 		}
@@ -71,7 +71,7 @@ public class GameLogic {
 		return fieldSize;
 	}
 
-    public int setValue(CellState value, int row, int col) {
+    public int setValue(CellState value, short row, short col) {
     	if ((row > ROWS || row < 0) || (col > COLS || col < 0)) {
 		    return 1;
 	    }
@@ -81,7 +81,7 @@ public class GameLogic {
 		}
     }
 
-    public CellState getValue(int row, int col) {
+    public CellState getValue(short row, short col) {
 		if ((row > ROWS || row < 0) || (col > COLS || col < 0)) {
 				//write custom exception here 
 				return field.get(row).get(col);
@@ -92,8 +92,8 @@ public class GameLogic {
 	}
 
 
-	private int countFlaggedCells() {
-		int count = (int) field.stream().flatMap(List::stream).filter(state -> state == CellState.FLAGGED || state == CellState.FLAGGED_MINE).count();
+	private short countFlaggedCells() {
+		short count = (short) field.stream().flatMap(List::stream).filter(state -> state == CellState.FLAGGED || state == CellState.FLAGGED_MINE).count();
 		return count;
 	}
 
@@ -106,7 +106,7 @@ public class GameLogic {
 		return gameOver;
 	}
 
-	public void toggleFlag(int row, int col) {
+	public void toggleFlag(short row, short col) {
 		CellState state = field.get(col).get(row);
 		if (null != state) switch (state) {
 			case EMPTY -> field.get(row).set(col, CellState.FLAGGED);
@@ -119,7 +119,7 @@ public class GameLogic {
 		updateCounter();
 	}
 	
-	private byte countNearbyMines(int row, int col) {
+	private byte countNearbyMines(short row, short col) {
 		byte count = 0;
 		for (int i = -1; i < 2; i++) {
 			for (int j = -1; j < 2; j++) {
@@ -136,7 +136,7 @@ public class GameLogic {
 		return count;
 	}
 
-	private byte countNearbyFlags(int row, int col) {
+	private byte countNearbyFlags(short row, short col) {
 		byte count = 0;
 		for (int i = -1; i < 2; i++) {
 			for (int j = -1; j < 2; j++) {
@@ -160,7 +160,7 @@ public class GameLogic {
 		}
 	}
 
-	public void openCell(int row, int col) {
+	public void openCell(short row, short col) {
 		if (gameOver) return;
 		CellState state = field.get(row).get(col);
 		if (null != state) switch (state) {
@@ -178,10 +178,19 @@ public class GameLogic {
 		}
 	}
 
-//	public List<byte[]> getCellsToOpenList() {
-//		List<byte[]> toOpen = new ArrayList<>();
-//
-//	}
+	public List<short[]> getCellsToOpenList() {
+		List<short[]> cellsToOpen = new ArrayList<>();
+		boolean[][] visitedCells = new boolean[ROWS][COLS];
+		Queue<short[]> queue = new ArrayDeque<>();
+
+
+		
+
+		
+		
+		
+		return cellsToOpen;
+	}
 //	public void OpenCell(int Row, int Col) {
 //		if (GameOver || GameField[Row][Col] == CellState.Flagged.ordinal() || GameField[Row][Col] == CellState.FlaggedMine.ordinal()) {return;}
 //		if (this.GameField[Row][Col] == CellState.ContainesMine.ordinal()) {
