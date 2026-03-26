4133fab2f0ece752629b7f2c1b2cb7593de5494b
Ebenbl4 <168077908+Ebenbl4@users.noreply.github.com>
2026-03-26 18:16:49 +0300

    Add methods for window render

--- src/minesweeperkurs/MinesweeperWindow.java
+++ src/minesweeperkurs/MinesweeperWindow.java
@@ -65,7 +65,7 @@ public class MinesweeperWindow extends JFrame{
 		short[] fieldSize = game.getFieldSize();
 		JPanel gameField = new JPanel(new GridLayout(fieldSize[0], fieldSize[1], 3, 3));
 		gameField.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
-		JButton[][] fieldButtons = createButtons(fieldSize[0], fieldSize[1], gameField);
+		JButton[][] fieldButtons = createButtons(fieldSize[0], fieldSize[1], gameField, game);
 		c.gridy = 1;
 		c.gridwidth = GridBagConstraints.REMAINDER;
 		c.weighty = 1.0;
@@ -74,10 +74,11 @@ public class MinesweeperWindow extends JFrame{
 		
 		panel.add(gameField, c);
 		
+		btnMenu.addActionListener(e -> switchPanel(createMenuPanel()));
 		return panel;
 	}
 
-	JButton[][] createButtons(short rows, short cols, JPanel grid) {
+	JButton[][] createButtons(short rows, short cols, JPanel grid, GameLogic game) {
 		JButton[][] buttons = new JButton[cols][cols];
 		for (short i = 0; i < rows; i++) {
 			for (short j = 0; j < cols; j++) {
@@ -85,24 +86,77 @@ public class MinesweeperWindow extends JFrame{
 				button.setPreferredSize(new Dimension(40,40));
 				button.setFont(new Font("Arial", Font.BOLD, 16));
 				button.setFocusPainted(false);
+				button.setBackground(Color.gray);
 				button.setMargin(new Insets(0, 0, 0, 0));
 				button.putClientProperty("row", i);
 				button.putClientProperty("col", j);
-//				button.addActionListener(e -> clickedOnCell(button));
-//				button.addMouseListener(new MouseAdapter() {
-//					@Override
-//					public void mousePressed(MouseEvent e) {
-//						if (SwingUtilities.isRightMouseButton(e)) {
-//						}
-//						else if (SwingUtilities.isMiddleMouseButton(e)) {
-//						}
-//					}
-//				});
+				button.addActionListener(e -> {
+					clickedOnCell(button, game);
+					renderAllCells(buttons, game);
+				});
+				button.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mousePressed(MouseEvent e) {
+						if (SwingUtilities.isRightMouseButton(e)) {
+						}
+						else if (SwingUtilities.isMiddleMouseButton(e)) {
+						}
+					}
+				});
 				buttons[i][j] = button;
 				grid.add(button);
 			}
 		}
 		return buttons;
+	}
+
+	private void clickedOnCell(JButton button, GameLogic game) {
+		short i = (short) button.getClientProperty("row");
+		short j = (short) button.getClientProperty("col");
+		game.openCell(i, j);
+		boolean[] status = game.getGameStatus();
+		if (status[0] && !status[1]) {
+			JOptionPane.showMessageDialog(this, "ЛМАО, ПОСОСИ!");
+		}
+		else if (status[0] && status[1]) {
+			JOptionPane.showMessageDialog(this, "ГОЙДА!");
+		}
+	}
+
+	private void renderAllCells(JButton[][] buttons, GameLogic game) {
+		short[] fieldSize = game.getFieldSize();
+		for (short i = 0; i < fieldSize[0]; i++) {
+			for (short j = 0; j < fieldSize[1]; j++) {
+				JButton button = buttons[i][j];
+				GameLogic.CellState state = game.getState(i, j);
+				switch (state) {
+					case OPENED -> {
+						byte nearbyMines = game.getNearbyMinesCount(i, j);
+						if (nearbyMines == 0) {
+							button.setText("");
+						}
+						else {
+							button.setText(String.valueOf(nearbyMines));
+						}
+						button.setBackground(Color.white);
+					}
+					case EMPTY -> {
+						button.setText("");
+						button.setBackground(Color.gray);
+					}
+					case CONTAINS_MINE -> {
+						button.setText("");
+						button.setBackground(Color.gray);
+					}
+					case FLAGGED -> {
+						button.setText("F");
+						button.setBackground(Color.gray);
+					}
+					default -> {
+					}
+				}
+			}
+		}
 	}
 }
 
