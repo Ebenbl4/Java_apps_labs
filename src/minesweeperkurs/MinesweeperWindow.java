61b6f14b8d6a4d0abfa7403992a33b50757eaf4a
Ebenbl4 <168077908+Ebenbl4@users.noreply.github.com>
2026-03-26 17:06:25 +0300

    Add code for gamefield

--- src/minesweeperkurs/MinesweeperWindow.java
+++ src/minesweeperkurs/MinesweeperWindow.java
@@ -46,10 +46,63 @@ public class MinesweeperWindow extends JFrame{
 		panel.add(btnExit);
 
 		btnExit.addActionListener(e -> System.exit(0));
+		btnOption1.addActionListener(e -> switchPanel(createGameFieldPanel()));
 		
 		
 		return panel;
 	}
+	
+	private JPanel createGameFieldPanel() {
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		JButton btnMenu = new JButton("To menu");
+		c.weightx = 0.5;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(btnMenu);
+		GameLogic game = new GameLogic();
+//		short[] fieldSize = game.getFieldSize();
+//		JPanel gameField = new JPanel(new GridLayout(fieldSize[0], fieldSize[1], 3, 3));
+//		gameField.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+//		JButton[][] fieldButtons = createButtons(fieldSize[0], fieldSize[1], gameField);
+//		c.gridy = 1;
+//		c.gridwidth = GridBagConstraints.REMAINDER;
+//		c.weighty = 1.0;
+//		c.fill = GridBagConstraints.BOTH;
+//		c.insets = new Insets(10, 5, 0, 5);
+//		
+//		panel.add(gameField, c);
+		
+		return panel;
+	}
 
+	JButton[][] createButtons(short rows, short cols, JPanel grid) {
+		JButton[][] buttons = new JButton[cols][cols];
+		for (short i = 0; i < rows; i++) {
+			for (short j = 0; j < cols; j++) {
+				JButton button = new JButton();
+				button.setPreferredSize(new Dimension(40,40));
+				button.setFont(new Font("Arial", Font.BOLD, 16));
+				button.setFocusPainted(false);
+				button.setMargin(new Insets(0, 0, 0, 0));
+				button.putClientProperty("row", i);
+				button.putClientProperty("col", j);
+//				button.addActionListener(e -> clickedOnCell(button));
+//				button.addMouseListener(new MouseAdapter() {
+//					@Override
+//					public void mousePressed(MouseEvent e) {
+//						if (SwingUtilities.isRightMouseButton(e)) {
+//						}
+//						else if (SwingUtilities.isMiddleMouseButton(e)) {
+//						}
+//					}
+//				});
+				buttons[i][j] = button;
+				grid.add(button);
+			}
+		}
+		return buttons;
+	}
 }
 
